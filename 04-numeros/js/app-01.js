daf57674ec850e058431a495541127e5be8a61ab
Alfonso Tort <alfonso.tort@anahuac.mx>
2022-09-21 01:19:48 -0500

    subir sección de 4 a 8

--- 04-numeros/js/app-01.js
+++ 04-numeros/js/app-01.js
@@ -1,11 +1,104 @@
-const numero6 = 20;
-const numero7 = "20";
+// const numero6 = 20;
+// const numero7 = "20";
 
-const numero1 = 30;
-const numero2 = 20;
-const numero3 = 20.2;
-const numero4 = 0.10213;
-const numero5 = -3;
+// const numero1 = 30;
+// const numero2 = 20;
+// const numero3 = 20.2;
+// const numero4 = 0.10213;
+// const numero5 = -3;
 
-const numero8 = new Number(20);
-console.log(numero8);
+// const numero8 = new Number(20);
+// console.log(numero8);
+
+// // Operaciones
+// let resultado;
+
+// // Suma
+// resultado = numero1 + numero2;
+// // Resta
+// resultado = numero1 - numero2;
+// // Mult
+// resultado = numero1 * numero2;
+// // Division
+// resultado = numero1 / numero2;
+// // Modulo
+// resultado = numero1 % numero2;
+
+// console.log(resultado);
+
+// Math es parte de la ventana global de JavaScript, tiene una serie de operaciones que pueden ser muy útiles y algunas actuan de forma algo extraña, asi que vamos a verlas.
+
+// let resultado;
+
+// // Pi
+// resultado = Math.PI;
+// // redondeo
+// resultado = Math.round(2.5);
+// // redondeo abajo o arriba (ceil o floor )
+// resultado = Math.ceil(2.2);
+// // Raiz cuadrada
+// resultado = Math.sqrt(144);
+// // Abssoluto
+// resultado = Math.abs(-300);
+// // Potencia
+// resultado = Math.pow(8, 3);
+// // Minimo
+// resultado = Math.min(3, 5, 1, 2, 9, 4, 2, -3);
+// // Max
+// resultado = Math.max(4, 1, 21, 4, 15, 5, 11, 5);
+// // Aleatorio
+// resultado = Math.random();
+// // Aleatorio dentro de un rango:
+// resultado = Math.floor(Math.random() * 30);
+
+// console.log(resultado);
+
+// // El Orden en que se ejecutan las operaciones en JavaScript es igual que en la escuela
+
+// // let resultado
+// // El orden de las operaciones
+
+// resultado = 20 + 30 * 2;
+// resultado = (20 + 30) * 2;
+
+// // 20% De descuento en un carrito de compra.
+// resultado = (20 + 10 + 30 + 40 + 50) * 0.2;
+
+// console.log(resultado);
+
+// ----------------------------------------
+
+// Incluir incremento por 1 y menos 1
+
+// let puntaje = 5;
+// puntaje++;
+// puntaje--;
+// ++puntaje;
+// --puntaje;
+
+// puntaje += 3;
+// puntaje -= 3;
+
+// console.log(puntaje);
+
+// funciones para convertir a números
+
+// const numero1 = "20";
+// const numero2 = "20.2";
+// const numero3 = "Uno";
+// const numero4 = 20;
+
+// console.log(numero1);
+
+// // Convertir de Strings a Números flotantes o Enteros
+
+// console.log(Number.parseInt(numero1)); // Convertir de String a Número
+// console.log(Number.parseFloat(numero2)); // Convertir a número con decimales
+// console.log(Number.parseInt(numero3));
+
+// // Revisar si un número es entero
+// console.log(Number.isInteger(numero4) ); // Revisar si un número es entero o no
+// console.log(Number.isInteger(numero3) ); //
+
+// // Convertir String a numero
+// console.log(numero4.toString());
